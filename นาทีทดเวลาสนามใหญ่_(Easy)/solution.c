a90e62bf357f6c8f091bac534174542927098204
Narawit Sandorn <nsandorn7@gmail.com>
2026-06-17 11:45:45 +0700

    Add world cup easy problems

---
+++ นาทีทดเวลาสนามใหญ่_(Easy)/solution.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    printf("%d", 90 + a + b);
+    return 0;
+}
---
+++ แต้มรอบแบ่งกลุ่ม_(Easy)/solution.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int w, d, l;
+    scanf("%d", &w);
+    scanf("%d", &d);
+    scanf("%d", &l);
+
+    printf("%d", w * 3 + d);
+    return 0;
+}
