956af2547a16ec363c51725f38c4fcb258c56e18
Kiatisak <kiatisakforstudy@gmail.com>
2026-07-02 03:26:53 +0700

    Add day 2 practice problems

---
+++ 08-Compare_Numbers/solution.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int a,b;
+
+    scanf("%d %d",&a,&b);
+
+    if(a>0 && b>0)
+        printf("Both Positive");
+    else
+        printf("Not Both Positive");
+
+    return 0;
+}
